1648ee40a3f9f617141b6785939922cb4f3ecefd
gpoc-bi <63909403+gpoc-bi@users.noreply.github.com>
2020-04-20 19:07:51 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession,SQLContext
+from pyspark.sql.types import *
+from pyspark import SparkConf
+from pyspark.context import SparkContext
+from pyspark.sql.functions import asc
+from pyspark.sql.functions import col
+from functools import reduce  # For Python 3.x
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import sha2, concat_ws
+from pyspark.sql import readwriter
+from pyspark.sql import utils 
+
+ 
+sc = SparkContext("local", "count app")
+SQLContext = SQLContext(sc)
+
+spark = SparkSession.builder. \
+    master('local'). \
+    appName('foo'). \
+    getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+ 
+#Reading source and lrf
+df_source = spark.read.csv("gs://bucket_for_sample-cluster/sample.csv", inferSchema= True , header= True)
+df_source.show()
+
+
+# Saving the data to BigQuery
+df_source.write.format('bigquery') \
+  .option('table', 'dataset_1.table_1') \
+  .option("temporaryGcsBucket","bucket_for_sample-cluster") \
+  .mode('append') \
+.save()
